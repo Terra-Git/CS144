4dbe092dda6b28a08a04183e7ddec3bbebb52176
Terra-Git <544708735@qq.com>
2023-12-15 16:25:08 +0800

    unwrap

--- etc/tests.cmake
+++ etc/tests.cmake
@@ -46,13 +46,13 @@ ttest(recv_reorder_more)
 ttest(recv_close)
 ttest(recv_special)
 
-add_custom_target (check0 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 12 -R 'webget|^byte_stream_')
+add_custom_target (check0 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 20 -R 'webget|^byte_stream_')
 
 add_custom_target (check_webget COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --timeout 20 -R 'webget')
 
 add_custom_target (check1 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 20 -R '^byte_stream_|^reassembler_')
 
-add_custom_target (check2 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 12 -R '^byte_stream_|^reassembler_|^wrapping|^recv')
+add_custom_target (check2 COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure --stop-on-failure --timeout 20 -R '^byte_stream_|^reassembler_|^wrapping|^recv')
 
 ###
 
--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,29 @@
 #include "wrapping_integers.hh"
 
+#include <cmath>
+#include <iostream>
+
 using namespace std;
 
-Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
+// 尽量避免隐式的类型转换
+Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point ) noexcept
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n % static_cast<uint64_t>( 1UL << 32 ) );
 }
 
-uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
+uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const noexcept
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  // check_point 相对 zero_point 的位置
+  Wrap32 check = zero_point + static_cast<uint32_t>(checkpoint % ( 1UL<<32 ));
+  // 区分两种情况，在左边或者右边
+  uint64_t left{},right{};
+  // check 在left左边
+  if( check.raw_value_ < raw_value_) {
+    left = (uint64_t)(check.raw_value_ + (uint32_t)( 1UL<<32 ) - raw_value_);
+    right = (uint64_t)(raw_value_ - check.raw_value_);
+  }else {
+    left = (uint64_t)(check.raw_value_ - raw_value_);
+    right = (uint64_t)(raw_value_ + (uint32_t)( 1UL<<32 ) - check.raw_value_);
+  }
+  return (left < right && checkpoint >= left) ? checkpoint - left : checkpoint + right;
 }
