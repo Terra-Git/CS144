3d02ea2b9f5a5ea1ea4a6c5f25411cd6f067ce90
Terra-Git <544708735@qq.com>
2023-12-18 18:00:44 +0800

    move

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,11 +4,9 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+
 class TCPSender
 {
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
-
 public:
   /* Construct TCP sender with given default Retransmission Timeout and possible ISN */
   TCPSender( uint64_t initial_RTO_ms, std::optional<Wrap32> fixed_isn );
@@ -31,4 +29,7 @@ public:
   /* Accessors for use in testing */
   uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
   uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
+private:
+  Wrap32 isn_;
+  uint64_t initial_RTO_ms_;
 };
