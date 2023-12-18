8d0cffc6e3f8495ed193f1f553f514583014665c
Terra-Git <544708735@qq.com>
2023-12-18 14:17:34 +0800

    check2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -1,18 +1,23 @@
 #include "tcp_receiver.hh"
+#include <iostream>
 
 using namespace std;
 
+/**
+ * 数据的第一个字节将具有 ISN + 1（ mod 2 >> 32）的序列号，初始化时需要＋1
+ * 逻辑开始和结束各占用一个序列号：除了确保接收所有数据字节外，TCP还确保流的开始和结束可靠接收
+ * 绝对序列号和流索引之间进行转换相对容易——只需加或减一 
+*/
 void TCPReceiver::receive( TCPSenderMessage message, Reassembler& reassembler, Writer& inbound_stream )
 {
-  // Your code here.
-  (void)message;
-  (void)reassembler;
-  (void)inbound_stream;
+  if(message.SYN){SYN = true; ISN = message.seqno;}
+  if(!SYN) {return ;}
+  reassembler.insert((message.seqno + message.SYN).unwrap(ISN, reassembler.bytes_pending()) - 1,message.payload,message.FIN,inbound_stream);
 }
 
 TCPReceiverMessage TCPReceiver::send( const Writer& inbound_stream ) const
 {
-  // Your code here.
-  (void)inbound_stream;
-  return {};
+  std::optional<Wrap32> ackno {};
+  if(SYN){ ackno = (ISN + 1 + inbound_stream.is_closed() + inbound_stream.bytes_pushed() % ( 1UL << 32)); }
+  return {ackno, (uint16_t)std::min(inbound_stream.available_capacity(), (uint64_t)UINT16_MAX)};
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -15,4 +15,7 @@ public:
 
   /* The TCPReceiver sends TCPReceiverMessages back to the TCPSender. */
   TCPReceiverMessage send( const Writer& inbound_stream ) const;
+private:
+  bool SYN{false};
+  Wrap32 ISN{0}; // 初始化序列号
 };
