73079a52c9dd06810847bd7a773b79664bcbdef1
Terra-Git <544708735@qq.com>
2023-12-19 10:19:38 +0800

    Timer class define

--- src/tcp_sender.hh
+++ src/tcp_sender.hh
@@ -4,6 +4,33 @@
 #include "tcp_receiver_message.hh"
 #include "tcp_sender_message.hh"
 
+#include <queue>
+#include <memory>
+
+enum class TimerStatus{ start, stop, error};
+
+class Timer
+{
+public:
+  Timer(uint64_t time):initial_RTO_ms_(time){}
+  void reset() noexcept {
+    RTO_ = initial_RTO_ms_;
+    is_start_ = true;
+  }
+
+  bool     is_start()const noexcept { return is_start_;}
+  void     time_start() noexcept { is_start_ = true; }
+  void     time_stop() noexcept { is_start_ = false; }
+  bool     is_expire()const noexcept { return RTO_ <= 0; }
+  void     tick(uint64_t time) noexcept { RTO_ -= time; }
+  uint64_t get_RTO() const {return RTO_; }
+  void     double_RTO() noexcept { RTO_ *= 2; }
+
+private:
+  const uint64_t   initial_RTO_ms_;
+  uint64_t         RTO_{0};
+  bool             is_start_{false};
+};
 
 class TCPSender
 {
@@ -30,6 +57,16 @@ public:
   uint64_t sequence_numbers_in_flight() const;  // How many sequence numbers are outstanding?
   uint64_t consecutive_retransmissions() const; // How many consecutive *re*transmissions have happened?
 private:
-  Wrap32 isn_;
-  uint64_t initial_RTO_ms_;
+  using MsgQueue =  std::queue<TCPSenderMessage>;
+
+  Wrap32    isn_;
+  uint64_t  initial_RTO_ms_;
+  bool      SYN_{false};
+  bool      FIN_{false};
+  uint64_t  receive_seqno_{};         // 已经确认的数据下标
+  uint64_t  next_seqno_{};            // 下一个发送的数据下标
+  uint64_t  retransmission_count{};  // 重传的message数量
+  uint16_t  windows_size_{};          // 窗口大小
+  MsgQueue  send_queue_{};            // 待发送队列
+  MsgQueue  outstanding_queue_{};     // 已发送待确认的队列
 };
